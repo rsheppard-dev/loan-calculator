9cf470b0736e9cc4489525b1ff857e3f9b5a4807
Roy Sheppard <rsheppard83@gmail.com>
2023-01-31 17:47:48 +0000

    init commit

--- src/main.ts
+++ src/main.ts
@@ -1,72 +1,137 @@
 import './style.css';
 
 // function to get values from user interface
-const getValues = (e: Event): void => {
+function getValues(e: Event): void {
 	// prevent default form behaviour (page refresh)
 	e.preventDefault();
 
 	// get elements from html page
-	const startValueInput = document.getElementById(
-		'startValue'
+	const loanAmountInput = document.getElementById(
+		'loan-amount'
 	) as HTMLInputElement;
-	const endValueInput = document.getElementById('endValue') as HTMLInputElement;
+	const termInput = document.getElementById('term') as HTMLInputElement;
+	const rateInput = document.getElementById('rate') as HTMLInputElement;
 
-	// get values from inputs and parse to integers
-	const startValue = parseInt(startValueInput.value);
-	const endValue = parseInt(endValueInput.value);
+	// get values from inputs
+	const loanAmount = parseInt(loanAmountInput.value);
+	const term = parseInt(termInput.value);
+	const rate = parseInt(rateInput.value);
 
-	// check values entered by user are valid numbers
-	if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
-		// call generateNumbers function with input values
-		const numbers = generateNumbers(startValue, endValue);
+	// clear any old errors
+	handleError();
 
-		// call displayNumbers function
-		displayNumbers(numbers);
-	} else {
-		// display error on page if invalid input
-		handleError('You can only enter numbers in the inputs above.');
-	}
-};
-
-// function to generate numbers between startValue and endValue
-const generateNumbers = (startValue: number, endValue: number): number[] => {
-	const numbers: number[] = [];
-
-	// loop through all numbers from startValue to endValue
-	for (let i = startValue; i <= endValue; i++) {
-		// add current number to end of numbers array
-		numbers.push(i);
+	// validate inputs are integers
+	if (
+		![loanAmount, term, rate].every((input: number): boolean =>
+			Number.isInteger(input)
+		)
+	) {
+		return handleError('All inputs must contain a valid number!');
 	}
 
-	return numbers;
-};
+	// call displayPayments function to output results
+	displayPayments(loanAmount, term, rate);
+}
 
-// function to display number range
-const displayNumbers = (numbers: number[]): void => {
-	// get table body from html document
+// function to create an array of numbers for each payment
+function getNumberOfPayments(term: number): number[] {
+	const payments: number[] = [];
+
+	// loop through each number from 1 to loan term
+	// add number to payments array
+	for (let i = 1; i <= term; i++) {
+		payments.push(i);
+	}
+
+	return payments;
+}
+
+// function to calculate monthly payment
+function getMonthlyPayment(
+	loanAmount: number,
+	term: number,
+	rate: number
+): number {
+	return (loanAmount * (rate / 1200)) / (1 - (1 + rate / 1200) ** -term);
+}
+
+// function to calculate interest payment
+function getInterestPayment(balance: number, rate: number): number {
+	return balance * (rate / 1200);
+}
+
+// function to calculate principle payment
+function getPrinciplePayment(
+	monthlyPayment: number,
+	interestPayment: number
+): number {
+	return monthlyPayment - interestPayment;
+}
+
+function displayPayments(loanAmount: number, term: number, rate: number): void {
+	// get dom elements
 	const tableBody = document.getElementById(
 		'results'
 	) as HTMLTableSectionElement;
+	const rowTemplate = document.getElementById(
+		'table-row-template'
+	) as HTMLTemplateElement;
 
-	let templateRows = '';
+	// create array of numbers based on number of payments
+	const payments = getNumberOfPayments(term);
 
-	// loop through all the numbers
-	for (let i = 0; i < numbers.length; i++) {
-		const number = numbers[i];
+	// get monthly payment
+	const monthlyPayment = getMonthlyPayment(loanAmount, term, rate);
 
-		// check if current number is even or odd and assign correct class
-		const className = number % 2 === 0 ? 'even' : 'odd';
+	// get start balance
+	let balance = loanAmount + getInterestPayment(loanAmount, rate);
+	let totalInterest = 0;
 
-		// update template rows
-		templateRows += `<tr><td class="${className}">${number}<td></tr>`;
-	}
+	// for each due payment...
+	payments.forEach((payment: number) => {
+		const interestPayment = getInterestPayment(balance, rate);
+		const principalPayment = getPrinciplePayment(
+			monthlyPayment,
+			interestPayment
+		);
 
-	// update html in table body
-	tableBody.innerHTML = templateRows;
-};
+		// increase interest total
+		totalInterest += interestPayment;
+
+		// reduce total balance
+		balance = balance - monthlyPayment;
+
+		// clone template to create new row
+		const tableRow = document.importNode(rowTemplate.content, true);
+
+		// create an array of columns in the row
+		const rowCols = tableRow.querySelectorAll('td');
+
+		// add month to first column
+		rowCols[0].innerText = payment.toString();
+
+		// add payment to second column
+		rowCols[1].innerText = monthlyPayment.toFixed(2);
+
+		// add principal to third column
+		rowCols[2].innerText = principalPayment.toFixed(2);
+
+		// add interest to forth column
+		rowCols[3].innerText = interestPayment.toFixed(2);
+
+		// add total interest to fifth column
+		rowCols[4].innerText = totalInterest.toFixed(2);
+
+		// add balance to sixth column
+		rowCols[5].innerText = balance.toFixed(2);
+
+		// add row to table
+		tableBody.appendChild(tableRow);
+	});
+}
 
 // function to display and hide error message
-const handleError = (message?: string): void => {
+function handleError(message?: string): void {
 	// get error box element from html
 	const errorBox = document.getElementById('error') as HTMLDivElement;
 
@@ -79,7 +144,7 @@ const handleError = (message?: string): void => {
 		errorBox.innerText = '';
 		errorBox.classList.add('d-none');
 	}
-};
+}
 
 // get button element
 const submitButton = document.getElementById('submit') as HTMLButtonElement;
